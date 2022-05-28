526459bfcad2a23a16cd70b48168013c2b435ed8
Alihassoon93 <dmatthew.1993@gmail.com>
2022-05-28 23:59:53 +0300

    user app

---
+++ users/models.py
@@ -0,0 +1,57 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+# inherits from AbstractUser(default user model)
+class User(AbstractUser):
+
+    """Custom User Model"""
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_CHINESE = "ch"
+
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENGLISH, "En"),
+        (LANGUAGE_CHINESE, "Ch"),
+    )
+
+    CURRENCY_USD = "usd"
+    CURRENCY_RMB = "rmb"
+
+    CURRENCY_CHOICES = (
+        (CURRENCY_USD, "USD"),
+        (CURRENCY_RMB, "RMB"),
+    )
+
+    avatar = models.ImageField(blank=True)
+    gender = models.CharField(
+        choices=GENDER_CHOICES,
+        max_length=10,
+        blank=True,
+    )
+
+    bio = models.TextField(default="", blank=True)
+    birthdate = models.DateField(null=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES,
+        max_length=2,
+        blank=True,
+    )
+
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES,
+        max_length=3,
+        blank=True,
+    )
+
+    superhost = models.BooleanField(default=False)
